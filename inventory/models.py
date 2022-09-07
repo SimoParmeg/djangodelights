f4e1a8777ddeaa3accaaf51d8988ccca5f38a31f
SimoParmeg <parmeggiani.simone@gmail.com>
2022-09-07 17:32:25 +0200

    created models for DB

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Ingredient(models.Model):
+  name = ''
+  quantity = ''
+  unit = ''
+  unit_price = ''
+
+class MenuItem(models.Model):
+  pass
+
+class RecipeRequirement(models.Model):
+  pass
+
+class Purchase(models.Model):
+  pass
+
