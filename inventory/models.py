469dea09361d1cf8d2f54c33736c220ac3b60c63
SimoParmeg <parmeggiani.simone@gmail.com>
2022-09-07 17:36:38 +0200

    added variables in Ingredient model

--- inventory/models.py
+++ inventory/models.py
@@ -2,10 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Ingredient(models.Model):
-  name = ''
-  quantity = ''
-  unit = ''
-  unit_price = ''
+    """
+    Represents a single ingredient in the restaurant's inventory
+    """
+    name = models.CharField(max_length=200, unique=True)
+    quantity = models.FloatField(default=0)
+    unit = models.CharField(max_length=200)
+    price_per_unit = models.FloatField(default=0)
 
 class MenuItem(models.Model):
   pass
