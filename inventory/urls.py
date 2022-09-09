88a7b58adc0f6b0d2871bcc0eb3303c640f6a3f7
SimoParmeg <parmeggiani.simone@gmail.com>
2022-09-09 14:19:01 +0200

    typo correction

--- inventory/urls.py
+++ inventory/urls.py
@@ -10,7 +10,8 @@ urlpatterns = [
     path('ingredients/<pk>/delete', views.DeleteIngredientView.as_view(), name="delete_ingredient"),
     path('ingredients/add', views.AddIngredientView.as_view(), name="add_ingredient"),
     path('menu/', views.MenuItemView.as_view(), name="menus"),
-    path('menu/add', views.AddMenuItemView.as_view(), name="add_menus"),
+    path('menu/add', views.AddMenuItemView.as_view(), name="add_menu"),
+    path('menu/<pk>/delete', views.AddMenuItemView.as_view(), name="delete_menu"),
     path('purchases/', views.PurchaseView.as_view(), name="purchases"),
     path('reports/', views.ReportView.as_view(), name="reports"),
 ]
